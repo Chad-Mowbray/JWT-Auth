15d88a0cd0aad30dcc1f17812aa1d40292100bb5
Chad-Mowbray <mowbray.chad@gmail.com>
2020-03-04 22:19:20 -0600

    almost have token as auth working

--- app/database/fakeDatabase.js
+++ app/database/fakeDatabase.js
@@ -2,22 +2,18 @@
 module.exports =  users = [
         {
             username: "computerBob",
-            email: "bob@bobsystems.com",
             password: "P@ssword"
         },
         {
             username: "superuser",
-            email: "super@user.com",
             password: "secret123"   
         },
         {
             username: "fozzybear",
-            email: "fozzy@bear.com",
             password: "opensesame"
         },
         {
             username: "a",
-            email: "fozzy@bear.com",
             password: "a"
         }
     ]
--- app/server.js
+++ app/server.js
@@ -21,37 +21,53 @@ const createSignedJWT = function(req, res) {
     const now = new Date()  
     const epochTime = Math.round(now.getTime() / 1000)
     const username = req.headers.cookie.split('; ').filter( pair => pair.split('=')[0] === 'username')[0].split('=')[1]
-    // const x = cookieSplit.filter( pair => pair.split('=')[0] === 'username').split('=')[1]
 
     let payload = {
         "jti": jti,
-        "exp": epochTime + 9999,                                           
+        "exp": epochTime + 60,                                           
         "iat": epochTime,
         "scope": "secretpage:read",
         "username": username
     }
-    console.log("PAYLOAD: ", payload)
+    // console.log("PAYLOAD: ", payload)
  
     let privateKey = fs.readFileSync( path.join(__dirname, "public", "certificate", "key.pem"))
     let token = jwt.sign(payload,{"key": privateKey, "passphrase": "1234"}, { algorithm: 'RS256' });  // passphrase is the password you use when generating the certificate
+    
+    for(let user of usersDatabase) {
+        if(payload["username"] === user.username) {
+            user.JWT = token
+        }
+    }
+    console.log(usersDatabase)
+
     res.cookie("userToken", token)
     return token
+}
+
+const verifyJWT = function(token) {
+    console.log("token in verifyJWT: ", token)
+    var cert = fs.readFileSync('public/certificate/cert.pem', "utf8");  // get public key
+    console.log("cert in verifyJWT: ", cert)
+
+    let checkedToken = jwt.verify(token, cert)
+    return checkedToken
 }
 
 // Delete all cookies
 
 function clearCookies(req, res) {
-    console.log('in clearCookies')
+    // console.log('in clearCookies')
     res.cookie("username", "; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;", {encode: String})
     res.cookie("password", "; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;", {encode: String})
 }
  
 const returnToken = function(req, res) {
-    console.log("in returnToken")
+    // console.log("in returnToken")
     // console.log(loggedIn(req))
     if(loggedIn(req)) {
-        console.log('in returnToken, in if')
-        console.log('about to clear cookies')
+        // console.log('in returnToken, in if')
+        // console.log('about to clear cookies')
         // clearCookies(req, res)
         let token = createSignedJWT(req, res)
         clearCookies(req, res)
@@ -68,27 +84,27 @@ const loggedIn = function(req) {
         return false
     } else {
         let isLoggedIn = req.headers.cookie.split('; ').filter( keyVal => keyVal.split("=")[1] === "true" )
-        console.log("in loggedIn.  Logged in? : ")
+        // console.log("in loggedIn.  Logged in? : ")
         return isLoggedIn ? true : false
     }
 }
  
   
 app.get('/', function(req, res) {
-    console.log('GET request to /...')
+    // console.log('GET request to /...')
     res.send("This is just an entrypoint. Go to /login to log in, or to /signup to signup")
 })
 
 app.get('/login', function(req, res) {
-    console.log('GET request to /login...')
+    // console.log('GET request to /login...')
     // console.log(loggedIn(req))
     res.render("login.html", {title: 'Login Page', numUsers: usersDatabase.length, isloggedIn: loggedIn(req)})
 })
 
 app.post('/login', function(req, res) {
-    console.log('POST request to /login...')
+    // console.log('POST request to /login...')
     if(usersDatabase.filter( (user) => user['username'] === req.body.username ).length === 1 && users.filter( (user) => user['password'] === req.body.password).length === 1) {
-        console.log('Valid user')
+        // console.log('Valid user')
         res.cookie("username", req.body.username)
         res.cookie("password", req.body.password)
         // res.cookie("loggedIn", "true")
@@ -101,7 +117,7 @@ app.post('/login', function(req, res) {
 })
 
 app.get('/signup', function(req, res) {
-    console.log('GET request to /signup...')
+    // console.log('GET request to /signup...')
     res.render("signup.html", {notSignedUpYet: true, title: 'Signup Page', numUsers: usersDatabase.length, isLoggedIn: loggedIn(req)})
 })
 
@@ -109,7 +125,6 @@ app.post('/signup', function(req, res) {
     if(usersDatabase.filter( (user) => user['username'] === req.body.username ).length === 0 && usersDatabase.filter( (user) => user['password'] === req.body.password).length === 0) {
         usersDatabase.push({
             username: req.body.username,
-            email: 'default@email.com',
             password: req.body.password
         })
         // console.log(usersDatabase)
@@ -122,7 +137,7 @@ app.post('/signup', function(req, res) {
 
 app.get('/token', function(req, res) {
     if(loggedIn(req)) {
-        console.log('Logged in at /token')
+        // console.log('Logged in at /token')
         const token = returnToken(req, res) 
         if(token.length > 1) {
             res.send('You should have a token now')
@@ -135,10 +150,18 @@ app.get('/token', function(req, res) {
 })
 
 app.get('/secretpage', function(req, res) {
-    console.log('GET request to /secretpage...')
-    // if token valid -> see page else not
+    // console.log('GET request to /secretpage...')
+    // console.log(req.headers.cookie)
+    const token = req.headers.cookie.split('; ').filter( pair => pair.split('=')[0] === 'userToken')[0].split('=')[1]
 
-    res.render("secretpage.html", {title: 'Secret Page', numUsers: usersDatabase.length, isLoggedIn: loggedIn(req)})
+    console.log("TOKEN in /secretpage", token)
+    if(verifyJWT(token)) {
+        res.render("secretpage.html", {title: 'Secret Page', numUsers: usersDatabase.length, isLoggedIn: loggedIn(req)})
+
+    } else {
+        res.send('You are not authorized to see /secretpage')
+    }
+
 })
  
 // app.post('/publickey', function(req, res) {
